49b9dd008f71df3eea644e4cdaf560ba7605456c
DayvsonDvs <dayvson_dvs@hotmail.com>
2022-10-14 15:13:23 -0300

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background: #43b883;
+  color: #fff;
+  outline: none;
+  border: none;
+}
+</style>
